9dd88f9fe96599a77a9bd77b1f894188ddfb3246
LunarX <gibran.chevalley@etu.unige.ch>
2020-04-17 15:25:06 +0200

    Added orchestrator with tips

---
+++ Assets/Orchestrator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orchestrator : MonoBehaviour
+{
+
+    // Tuple.Create(var1, var2, var3, ...)
+    // if (NeedNewTargets)
+    // var list = GenerateTargets()
+    // var newTargets = list.Item1 
+    // var directions = lsit.Item2
+    // for i in newTargets
+    //  stack[direction[i]] = newTargets[i]
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
