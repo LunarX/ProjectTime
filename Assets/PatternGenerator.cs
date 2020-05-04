8cd8f702f8bb8a31efe96403505d3033f17254a7
LunarX <gibran.chevalley@etu.unige.ch>
2020-05-04 17:46:46 +0200

    Made patterns parametrable in paterns.txt

--- Assets/PatternGenerator.cs
+++ Assets/PatternGenerator.cs
@@ -8,42 +8,52 @@ public class PatternGenerator
     private List<List<(int[] directions, float timeOffset)>> patterns;
     private System.Random rand;
 
+    private readonly Dictionary<string, int> keywords = new Dictionary<string, int> { { "UP", GameManager.UP }, { "DOWN", GameManager.DOWN }, { "LEFT", GameManager.LEFT }, { "RIGHT", GameManager.RIGHT } };
+
     public PatternGenerator()
     {
         rand = new System.Random(3);
         patterns = new List<List<(int[] directions, float timeOffset)>>();
 
-        patterns.Add(
-            new List<(int[] directions, float timeOffset)>
-            {
-                (directions: new int[] { GameManager.UP }, 0f),
-                (directions: new int[] { GameManager.UP }, 0.5f),
-                (directions: new int[] { GameManager.LEFT, GameManager.RIGHT }, 1f)
-            }
-        );
+        var file = Resources.Load("patterns") as TextAsset;
+        var patternsSource = file.text;
 
-        patterns.Add(
-            new List<(int[] directions, float timeOffset)>
-            {
-                (directions: new int[] { GameManager.UP }, 0f),
-                (directions: new int[] { GameManager.RIGHT }, 0.25f),
-                (directions: new int[] { GameManager.DOWN }, 0.5f),
-                (directions: new int[] { GameManager.LEFT }, 0.75f)
-            }
-        );
+        string[] stringSeparators = new string[] { "\r\n\r\n", "\n\n" };
+        var textPatterns = patternsSource.Split(stringSeparators, StringSplitOptions.None);
 
-        patterns.Add(
-            new List<(int[] directions, float timeOffset)>
+        foreach(var p in textPatterns)
+        {
+            if(!String.IsNullOrEmpty(p))
             {
-                (directions: new int[] { GameManager.UP }, 0f),
-                (directions: new int[] { GameManager.LEFT }, 0.25f),
-                (directions: new int[] { GameManager.DOWN }, 0.5f),
-                (directions: new int[] { GameManager.RIGHT }, 0.75f),
-                (directions: new int[] { GameManager.DOWN }, 1f),
-                (directions: new int[] { GameManager.LEFT }, 1.25f),
-                (directions: new int[] { GameManager.UP }, 1.5f)
+                List<(int[] directions, float timeOffset)> pattern = new List<(int[] directions, float timeOffset)>();
+
+                var lines = p.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+                foreach (var line in lines)
+                {
+                    if (!String.IsNullOrEmpty(line))
+                    {
+                        List<int> dir = new List<int>();
+                        float offset = -1f;
+
+                        var values = line.Split(' ');
+                        for(int i = 0; i<values.Length - 1; i++)
+                        {
+                            //Debug.Log(i + " : " + values[i]);
+                            dir.Add(keywords[values[i]]);
+                        }
+
+                        offset = float.Parse(values[values.Length - 1]);
+                        //Debug.Log("[" + dir.ToArray() + "]" + " - " + offset);
+
+                        pattern.Add((directions: dir.ToArray(), timeOffset: offset));
+                    }
+                }
+
+                patterns.Add(pattern);
+                //Debug.Log("====");
             }
-        );
+            
+        }
     }
 
     public List<(int[], float)> GetRandomPattern()
