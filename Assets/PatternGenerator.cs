cbf09a4e44e7e29b630d50236ad5d2b5205dce57
LunarX <gibran.chevalley@etu.unige.ch>
2020-05-04 18:05:03 +0200

    Commented out new pattern generator implementation and put back the old one for now

--- Assets/PatternGenerator.cs
+++ Assets/PatternGenerator.cs
@@ -8,55 +8,83 @@ public class PatternGenerator
     private List<List<(int[] directions, float timeOffset)>> patterns;
     private System.Random rand;
 
-<<<<<<< HEAD
     private readonly Dictionary<string, int> keywords = new Dictionary<string, int> { { "UP", GameManager.UP }, { "DOWN", GameManager.DOWN }, { "LEFT", GameManager.LEFT }, { "RIGHT", GameManager.RIGHT } };
-=======
->>>>>>> 75f38b85f76bd75e784486c41eaa5164306ee46f
 
     public PatternGenerator()
     {
         rand = new System.Random(3);
         patterns = new List<List<(int[] directions, float timeOffset)>>();
 
-        var file = Resources.Load("patterns") as TextAsset;
-        var patternsSource = file.text;
+        //var file = Resources.Load("patterns") as TextAsset;
+        //var patternsSource = file.text;
 
-        string[] stringSeparators = new string[] { "\r\n\r\n", "\n\n" };
-        var textPatterns = patternsSource.Split(stringSeparators, StringSplitOptions.None);
+        //string[] stringSeparators = new string[] { "\r\n\r\n", "\n\n" };
+        //var textPatterns = patternsSource.Split(stringSeparators, StringSplitOptions.None);
 
-        foreach(var p in textPatterns)
-        {
-            if(!String.IsNullOrEmpty(p))
+        //foreach(var p in textPatterns)
+        //{
+        //    if(!String.IsNullOrEmpty(p))
+        //    {
+        //        List<(int[] directions, float timeOffset)> pattern = new List<(int[] directions, float timeOffset)>();
+
+        //        var lines = p.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+        //        foreach (var line in lines)
+        //        {
+        //            if (!String.IsNullOrEmpty(line))
+        //            {
+        //                List<int> dir = new List<int>();
+        //                float offset = -1f;
+
+        //                var values = line.Split(' ');
+        //                for(int i = 0; i<values.Length - 1; i++)
+        //                {
+        //                    //Debug.Log(i + " : " + values[i]);
+        //                    dir.Add(keywords[values[i]]);
+        //                }
+
+        //                offset = float.Parse(values[values.Length - 1]);
+        //                //Debug.Log("[" + dir.ToArray() + "]" + " - " + offset);
+
+        //                pattern.Add((directions: dir.ToArray(), timeOffset: offset));
+        //            }
+        //        }
+
+        //        patterns.Add(pattern);
+        //        //Debug.Log("====");
+        //    }
+        //}
+
+        patterns.Add(
+            new List<(int[] directions, float timeOffset)>
             {
-                List<(int[] directions, float timeOffset)> pattern = new List<(int[] directions, float timeOffset)>();
-
-                var lines = p.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-                foreach (var line in lines)
-                {
-                    if (!String.IsNullOrEmpty(line))
-                    {
-                        List<int> dir = new List<int>();
-                        float offset = -1f;
-
-                        var values = line.Split(' ');
-                        for(int i = 0; i<values.Length - 1; i++)
-                        {
-                            //Debug.Log(i + " : " + values[i]);
-                            dir.Add(keywords[values[i]]);
-                        }
-
-                        offset = float.Parse(values[values.Length - 1]);
-                        //Debug.Log("[" + dir.ToArray() + "]" + " - " + offset);
-
-                        pattern.Add((directions: dir.ToArray(), timeOffset: offset));
-                    }
-                }
-
-                patterns.Add(pattern);
-                //Debug.Log("====");
+                (directions: new int[] { GameManager.UP }, 0f),
+                (directions: new int[] { GameManager.UP }, 0.5f),
+                (directions: new int[] { GameManager.LEFT, GameManager.RIGHT }, 1f)
             }
-            
-        }
+        );
+
+        patterns.Add(
+            new List<(int[] directions, float timeOffset)>
+            {
+                (directions: new int[] { GameManager.UP }, 0f),
+                (directions: new int[] { GameManager.RIGHT }, 0.25f),
+                (directions: new int[] { GameManager.DOWN }, 0.5f),
+                (directions: new int[] { GameManager.LEFT }, 0.75f)
+            }
+        );
+
+        patterns.Add(
+            new List<(int[] directions, float timeOffset)>
+            {
+                (directions: new int[] { GameManager.UP }, 0f),
+                (directions: new int[] { GameManager.LEFT }, 0.25f),
+                (directions: new int[] { GameManager.DOWN }, 0.5f),
+                (directions: new int[] { GameManager.RIGHT }, 0.75f),
+                (directions: new int[] { GameManager.DOWN }, 1f),
+                (directions: new int[] { GameManager.LEFT }, 1.25f),
+                (directions: new int[] { GameManager.UP }, 1.5f)
+            }
+        );
     }
 
     // ..?
